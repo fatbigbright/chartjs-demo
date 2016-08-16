5b430b80af7ceda6ccaf933eb25a515ab726f655
fatbigbright <bigbrightf@gmail.com>
2016-08-16 15:08:11 +0800

    update for double yAxis

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 require.config({
   paths: {
     'jquery': '../node_modules/jquery/dist/jquery.min',
@@ -6,44 +7,54 @@ require.config({
 });
 
 define(['jquery', 'Chart'], function($, Chart){
+  */
+$(document).ready(function(){
   if($('canvas.can').length <= 0) return;
   var canvas = $('canvas.can')[0];
 
   var ctx = canvas.getContext('2d');
+  Chart.defaults.global.hover.mode = 'single';
   var chart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['00:00', '00:15', '00:30', '00:45', '01:00', '01:15'],
       datasets: [
         {
-          label: '# of Votes',
+          label: 'Humidity',
+          yAxisID: 'HT',
+          data: [0.75, 0.8, 0.76, 0.85, 0.9, 0.68],
+          backgroundColor: 'rgba(200, 99, 0, 0.2)',
+          borderColor: 'rgba(255, 0, 0, 1)',
+          borderWidth: 1
+        },
+        {
+          label: 'Temperature',
+          yAxisID: 'TT',
           data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
+          backgroundColor: 'rgba(0, 99, 200, 0.2)',
+          borderColor: 'rgba(00, 00, 255, 1)',
           borderWidth: 1
         }
       ]
     },
     options: {
       responsive: false,// to prevent the canvas to strech
+      hover: {
+        mode: 'label'
+      },
       scales: {
         yAxes: [
           {
+            id: "TT",
+            position: "left",
             ticks: {
+              beginAtZero: true
+            }
+          },
+          {
+            id: "HT",
+            position: "right",
+            ticks:{
               beginAtZero: true
             }
           }
